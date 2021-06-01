0b8657b909beb048fb26abe7cbf960e3a834d133
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-01 23:56:45 +0000

    linting this file and made corrections in the questions

--- survey.js
+++ survey.js
@@ -6,35 +6,29 @@ const rl = readline.createInterface({
 });
 let information = [];
 
-rl.question('What\'s your name? Nicknames are also acceptable :)', (answer) => {
+rl.question(`What's your name? Nicknames are also acceptable :)`, (answer) => {
   information.push(`My name is ${answer}. `);
 
-  rl.question(`What\'s an activity you like doing?`, (answer) =>
-  {
+  rl.question(`What's an activity you like doing?`, (answer) => {
     information.push(`I love ${answer}`);
 
-    rl.question('What do you listen to while doing that?', (answer) => 
-    {
+    rl.question('What do you listen to while doing that?', (answer) => {
       information.push(`, listening to ${answer}`);
 
-      rl.question('Which meal is your favourite (eg: dinner, brunch, etc.)', (answer) => 
-      {
+      rl.question('Which meal is your favourite (eg: dinner, brunch, etc.)', (answer) => {
         information.push(`, my favorite meal of the day is ${answer}`);
 
-        rl.question('What\'s your favourite thing to eat for that meal?', (answer) => 
-        {
+        rl.question('What\'s your favourite thing to eat for that meal?', (answer) => {
           information.push(`, I love eating ${answer}`);
 
-          rl.question(`Which sport is your absolute favourite?`, (answer) => 
-          {
+          rl.question(`Which sport is your absolute favourite?`, (answer) => {
             information.push(` and I also enjoy playing ${answer}`);
 
-            rl.question(`What is your superpower? In a few words, tell us what you are amazing at!`, (answer) => 
-            {
-              information.push(`. My superpower is ${answer}.`)
-              let profile = information.join('')
+            rl.question(`What is your superpower? In a few words, tell us what you are amazing at!`, (answer) => {
+              information.push(`. My superpower is ${answer}.`);
+              let profile = information.join('');
 
-              console.log(profile)
+              console.log(profile);
               rl.close();
             });
           });
@@ -42,4 +36,4 @@ rl.question('What\'s your name? Nicknames are also acceptable :)', (answer) => {
       });
     });
   });
-})
+});
