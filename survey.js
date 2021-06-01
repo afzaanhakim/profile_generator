f04303fe407c0dbd81b7f6cb9c9cc4b98c3d060f
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-01 23:34:54 +0000

    implemented questions now testing

--- survey.js
+++ survey.js
@@ -4,11 +4,41 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-let information = []
+let information = [];
+
 rl.question('What\'s your name? Nicknames are also acceptable :)', (answer) => {
   information.push(`My name is ${answer}`);
-  rl.question('What\'s an activity you like doing?)', (answer) => {
-    information.push(`I like playing ${answer}`)
- 
-  rl.close();
-});})
+
+  rl.question(`What\'s an activity you like doing?`, (answer) =>
+  {
+    information.push(`I like ${answer}`);
+
+    rl.question('What do you listen to while doing that?', (answer) => 
+    {
+      information.push(`I love ${answer} music`);
+
+      rl.question('Which meal is your favourite (eg: dinner, brunch, etc.)', (answer) => 
+      {
+        information.push(`My favorite meal is  ${answer}`);
+
+        rl.question('What\'s your favourite thing to eat for that meal?', (answer) => 
+        {
+          information.push(`I like eating ${answer}`);
+
+          rl.question(`Which sport is your absolute favourite?`, (answer) => 
+          {
+            information.push(`${answer} is my absolute favorite!`);
+
+            rl.question(`What is your superpower? In a few words, tell us what you are amazing at!`, (answer) => 
+            {
+              information.push(`My superpower is ${answer} is my absolute favorite!`)
+
+              
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
+})
