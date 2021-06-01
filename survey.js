2477f2289d2d5fbb4149dcdc578022766d9d2607
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-01 23:53:17 +0000

    implemented all questions and pushed the resulting answers to an array to consoole.log that description

--- survey.js
+++ survey.js
@@ -7,33 +7,34 @@ const rl = readline.createInterface({
 let information = [];
 
 rl.question('What\'s your name? Nicknames are also acceptable :)', (answer) => {
-  information.push(`My name is ${answer}`);
+  information.push(`My name is ${answer}. `);
 
   rl.question(`What\'s an activity you like doing?`, (answer) =>
   {
-    information.push(`I like ${answer}`);
+    information.push(`I love ${answer}`);
 
     rl.question('What do you listen to while doing that?', (answer) => 
     {
-      information.push(`I love ${answer} music`);
+      information.push(`, listening to ${answer}`);
 
       rl.question('Which meal is your favourite (eg: dinner, brunch, etc.)', (answer) => 
       {
-        information.push(`My favorite meal is  ${answer}`);
+        information.push(`, my favorite meal of the day is ${answer}`);
 
         rl.question('What\'s your favourite thing to eat for that meal?', (answer) => 
         {
-          information.push(`I like eating ${answer}`);
+          information.push(`, I love eating ${answer}`);
 
           rl.question(`Which sport is your absolute favourite?`, (answer) => 
           {
-            information.push(`${answer} is my absolute favorite!`);
+            information.push(` and I also enjoy playing ${answer}`);
 
             rl.question(`What is your superpower? In a few words, tell us what you are amazing at!`, (answer) => 
             {
-              information.push(`My superpower is ${answer} is my absolute favorite!`)
+              information.push(`. My superpower is ${answer}.`)
+              let profile = information.join('')
 
-              
+              console.log(profile)
               rl.close();
             });
           });
